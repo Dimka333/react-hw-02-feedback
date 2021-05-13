887874a24bc98bd293a52c6f9135d88372e0ba26
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2021-05-13 20:04:19 +0300

    ДЗ React №2 Feedback
    Правки (состояние до рефакторинга)

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -7,59 +7,68 @@ class Feedback extends Component {
             good: 0,
             neutral: 0,
             bad: 0
-        };
-    hendleGoodFeedback = () => {
-        this.setState((prevState) => {
-            return {
-                good: prevState.good + 1,
-            };
-        });
     };
+    
+    handleFeedback = (e) => {
+        this.setState((prevState) => ({
+            [e]: prevState[e] + 1,
+        }));
+    }
+    
 
-    hendleNeutralFeedback = () => {
-        this.setState((prevState) => {
-            return {
-                neutral: prevState.neutral + 1,
-            };
-        });
-    };
 
-    hendleBadFeedback = () => {
-        this.setState((prevState) => {
-            return {
-                bad: prevState.bad + 1,
-            };
-        });
-    };
+    // handleGoodFeedback = () => {
+    //     this.setState((prevState) => {
+    //         return {
+    //             good: prevState.good + 1,
+    //         };
+    //     });
+    // };
 
-//     hendleTotalFeedback = () => {
-//         return {
-//          this.state.bad + this.state.good + this.state.neutral;
-//     } 
-//   };
+    // handleNeutralFeedback = () => {
+    //     this.setState((prevState) => {
+    //         return {
+    //             neutral: prevState.neutral + 1,
+    //         };
+    //     });
+    // };
+
+    // handleBadFeedback = () => {
+    //     this.setState((prevState) => {
+    //         return {
+    //             bad: prevState.bad + 1,
+    //         };
+    //     });
+    // };
+
+    countTotalFeedback = () => {
+        return  this.state.bad + this.state.good + this.state.neutral;
+
+  };
     
     
-    hendlePositiveFeedback = () => {
-        const total = this.hendleTotalFeedback();
+    countPositiveFeedbackPercentage = () => {
+        const total = this.countTotalFeedback();
         if (total === 0) {
             return 0;
         }
-        return ((this.state.good / total) * 100)
+        return Math.round(((this.state.good / total) * 100))
    }
     
     render() {
         return (
             <div>
             <h1>Please leave feedback</h1>
-            <button type='button' onClick={this.hendleGoodFeedback}>Good</button>
-            <button type='button' onClick={this.hendleNeutralFeedback}>Neutral</button>
-            <button type='button' onClick={this.hendleBadFeedback}>Bad</button>
+            <button type='button' id='good' onClick={() => this.handleFeedback('good')}>Good</button>
+            <button type='button' id='neutral' onClick={() => this.handleFeedback('neutral')}>Neutral</button>
+            <button type='button' id='bad' onClick={() => this.handleFeedback('bad')}>bad</button>
+            
             <span>Statistics</span>
                 <p>Good:{this.state.good}</p>
                 <p>Neutral:{this.state.neutral}</p>
                 <p>Bad:{this.state.bad}</p>
-                <p>Total:{this.hendleTotalFeedback}</p>
-                <p>Positive feedback: {this.hendlePositiveFeedback}</p>
+                <p>Total:{this.countTotalFeedback()}</p>
+                <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
                 </div>
        )
    }
